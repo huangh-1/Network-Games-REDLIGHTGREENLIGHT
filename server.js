3aac8a49608bbc056c842ecaa274ac34a5e383d9
Hannah Huang <157334458+huangh-1@users.noreply.github.com>
2025-12-12 01:03:56 +0000

    boop

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 "use strict";
+
+const express = require("express");
 const socketIO = require("socket.io");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, "index.html");
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+const io = socketIO(server);
+
+let userCount = 0;
+let playerObjects = []; 
 
 io.on("connection", (socket) => {
   userCount++;
